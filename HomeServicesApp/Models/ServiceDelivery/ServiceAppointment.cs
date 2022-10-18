84622c5f2c8d38c817577eede7ef61a22eda4fa6
saadiyashraf <69361876+saadiyashraf@users.noreply.github.com>
2022-10-18 16:24:26 +0530

    Add files via upload

---
+++ HomeServicesApp/Models/ServiceDelivery/ServiceAppointment.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HomeServicesApp.Models.ServiceDelivery
+{
+    public class ServiceAppointment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("service_delivery_offer_id")]
+
+        public int service_delivery_offer_id { get; set; }
+
+        public DateTime service_deliver_on { get; set; }
+
+        public TimeOnly service_start_time  { get; set; }
+
+        public TimeOnly service_end_time { get; set; }
+    }
+}
---
+++ HomeServicesApp/Models/Services/ServiceCategory.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeServicesApp.Models.services
+{
+    public class ServiceCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? category_name { get; set; }
+
+    }
+}
+
